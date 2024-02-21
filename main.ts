c87042767ac5e286e961445faa9320bf0f79c306
martinbatrus27sthurmizdcom <149058610+martinbatrus27sthurmizdcom@users.noreply.github.com>
2024-02-21 15:04:02 +1100

    Update pxt.json, main.blocks, main.ts, pxt.json

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+let _0101001001000101010100101001001 = 0
 input.onButtonPressed(Button.A, function () {
+    if (true) {
+        basic.showString("" + (1 % 1))
+        radio.sendString("1")
+    } else if (false) {
+        radio.setGroup(1)
+        led.unplot(4, 2565337)
+        _0101001001000101010100101001001 += 1
+    } else if (false) {
+    	
+    } else {
+    	
+    }
     radio.sendString("1")
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString("Hello!")
-    radio.sendString("1")
-})
-radio.setGroup(1)
